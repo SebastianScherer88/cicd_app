0a04462728dc13bcc59bcc1ca05fcdae16db6bc2
bettmensch88 <sebastian.scherer@yahoo.de>
2022-05-21 17:03:10 +0100

    updated main script to run api from script

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ Created on Sat May 21 11:52:04 2022
 """
 
 from typing import Union
-
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -20,3 +20,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
