4cc398c4017dcb5959425c15f298ef68e955f2ed
bettmensch88 <sebastian.scherer@yahoo.de>
2022-05-21 16:54:03 +0100

    first commit

---
+++ app/main.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 21 11:52:04 2022
+
+@author: bettmensch
+"""
+
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
