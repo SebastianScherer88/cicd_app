20871c903cc96c1637ac8a43bd448d7dcb7cdb7e
bettmensch88 <sebastian.scherer@yahoo.de>
2022-05-23 23:47:53 +0100

    fixed integration test. removed api run statements from unit test run commands since not needed

--- tests/tests_integration/test_endpoints.py
+++ tests/tests_integration/test_endpoints.py
@@ -18,7 +18,7 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": generate_message()}
 
-@pytest.mark.parametrize('item_id,expected_item_id',[(1,1),(10,10),(-1,-1),(200,200)])
+@pytest.mark.parametrize('item_id',[1,10,-1,200])
 def test_item_endpoint(item_id):
     response = requests.get(f'http://127.0.0.1:{port}/items/{item_id}')
     assert response.status_code == 200
